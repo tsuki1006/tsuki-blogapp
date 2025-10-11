e122d9fb91f4b72c43a056f366357862a67361af
LAPTOP-H1OO9GCD\Yamamoto　Natsuki <caiyueshanben256@gmail.com>
2025-10-11 17:31:13 +0900

    homeへのリンクを追加

--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,10 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
-  get '/' => 'home#index'
+
+  root to: 'home#index'
+  # get '/' => 'home#index'
+
   get '/about' => 'home#about'
+
 end
