482727f72d6af6b692b50f3214d56f6ca7b4751b
LAPTOP-H1OO9GCD\Yamamoto　Natsuki <caiyueshanben256@gmail.com>
2025-10-12 10:57:48 +0900

    DBの値を表示する

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,6 @@
 class HomeController < ApplicationController
     def index
-        # render 'home/index'
-        @title = "デイトラ"
+        @article = Article.first
     end
     def about
     end
