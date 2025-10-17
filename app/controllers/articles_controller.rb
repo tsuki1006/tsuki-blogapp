d8cce5969c3b9e95cee404a9f888c874a9b47c6b
LAPTOP-H1OO9GCD\Yamamoto　Natsuki <caiyueshanben256@gmail.com>
2025-10-17 17:21:31 +0900

    createアクションを追加

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -10,4 +10,18 @@ class ArticlesController < ApplicationController
     def new
         @article = Article.new
     end
+
+    def create
+        @article = Article.new(article_params) #データを保存する場所を用意し、データを入れる
+        if @article.save  # データを保存する
+            redirect_to article_path(@article)  # 保存したらそのページに飛ぶ
+        else
+            render :new, status: :unprocessable_entity # new.html.erb を表示（model: @articleにはデータが一時的に保持された@articleが使われるため、データが入ったままのページが表示される）
+        end
+    end
+
+    private
+    def article_params
+        params.require(:article).permit(:title, :content)
+    end
 end
