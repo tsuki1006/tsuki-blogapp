b2186f797a820f7bcaabc990881daec66b6503bb
LAPTOP-H1OO9GCD\Yamamoto　Natsuki <caiyueshanben256@gmail.com>
2025-12-15 14:52:42 +0900

    良いねの状態をapiで取得

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -8,10 +8,10 @@ import $ from 'jquery'
 import axios from 'axios'
 
 document.addEventListener("turbo:load", () => {
-  $('.article_title').on('click', () => {
-    axios.get('/')
-      .then((response) => {
-        console.log(response);
-      });
-  });
+  const dataset = $('#article-show').data();
+  const articleId = dataset.articleId;
+  axios.get(`/articles/${articleId}/like`)
+    .then((response) => {
+      console.log(response);
+    });
 });
