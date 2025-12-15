a2a09b2e7cb3cc7d74906f227237e58cf4582c4b
LAPTOP-H1OO9GCD\Yamamoto　Natsuki <caiyueshanben256@gmail.com>
2025-12-15 22:17:57 +0900

    ハートの表示をコントロールするfuncitonを定義。可読性のため。

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -7,16 +7,21 @@ import "@rails/actiontext";
 import $ from 'jquery'
 import axios from 'axios'
 
+const handleHeartDisplay = (hasLiked) => {
+  if (hasLiked) {
+    $('.active-heart').removeClass('hidden');
+  } else {
+    $('.inactive-heart').removeClass('hidden');
+  };
+};
+
 document.addEventListener("turbo:load", () => {
   const dataset = $('#article-show').data();
   const articleId = dataset.articleId;
+
   axios.get(`/articles/${articleId}/like`)
     .then((response) => {
       const hasLiked = response.data.hasLiked;
-      if (hasLiked) {
-        $('.active-heart').removeClass('hidden');
-      } else {
-        $('.inactive-heart').removeClass('hidden');
-      };
+      handleHeartDisplay(hasLiked)
     });
 });
