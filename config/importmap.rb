83e92212304caeff5645f64b920a03f2a738efd7
LAPTOP-H1OO9GCD\Yamamoto　Natsuki <caiyueshanben256@gmail.com>
2025-12-15 15:17:12 +0900

    rubocop

--- config/importmap.rb
+++ config/importmap.rb
@@ -7,5 +7,5 @@ pin '@hotwired/stimulus-loading', to: 'stimulus-loading.js'
 pin_all_from 'app/javascript/controllers', under: 'controllers'
 pin 'trix'
 pin '@rails/actiontext', to: 'actiontext.esm.js'
-pin "jquery" # @3.7.1
-pin "axios", to: "https://cdn.skypack.dev/axios@1.13.2"
+pin 'jquery' # @3.7.1
+pin 'axios', to: 'https://cdn.skypack.dev/axios@1.13.2'
