08acc924ca38f908a464015fb8d836650e19499e
LAPTOP-H1OO9GCD\Yamamoto　Natsuki <caiyueshanben256@gmail.com>
2025-12-18 15:44:06 +0900

    Ajaxでコメントを追加

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -13,12 +13,9 @@ class CommentsController < ApplicationController
     def create
         article = Article.find(params[:article_id])
         @comment = article.comments.build(comment_params)
-        if @comment.save
-            redirect_to article_path(article), notice: 'コメントを追加しました'
-        else
-            flash.now[:error] = 'コメントできませんでした'
-            render :new, status: :unprocessable_entity
-        end
+        @comment.save!
+
+        render json: @comment
     end
 
     private
