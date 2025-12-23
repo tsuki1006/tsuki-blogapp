830bbf7a2d45b355c09ab98d2b488507e21ca7d3
LAPTOP-H1OO9GCD\Yamamoto　Natsuki <caiyueshanben256@gmail.com>
2025-12-23 16:35:17 +0900

    namespace, scopeに従いcontrollerを変更

--- app/javascript/article.js
+++ app/javascript/article.js
@@ -31,7 +31,7 @@ document.addEventListener("turbo:load", () => {
   const dataset = $('#article-show').data();
   const articleId = dataset.articleId;
 
-  axios.get(`/articles/${articleId}/comments`)
+  axios.get(`/api/articles/${articleId}/comments`)
     .then((response) => {
       const comments = response.data;
       comments.forEach((comment) => {
@@ -46,7 +46,7 @@ document.addEventListener("turbo:load", () => {
     if (!content) {
       window.alert('コメントを入力してください')
     } else {
-      axios.post(`/articles/${articleId}/comments`, {
+      axios.post(`/api/articles/${articleId}/comments`, {
         comment: {content: content}
       })
         .then((res) => {
@@ -57,7 +57,7 @@ document.addEventListener("turbo:load", () => {
     }
   });
 
-  axios.get(`/articles/${articleId}/like`)
+  axios.get(`/api/articles/${articleId}/like`)
     .then((response) => {
       const hasLiked = response.data.hasLiked;
       handleHeartDisplay(hasLiked)
